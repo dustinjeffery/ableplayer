8ff1e6b1724a26577968802f04fbfe270fe443eb
Dustin Jeffery <dustin.m.jeffery@gmail.com>
2024-12-12 22:40:21 -0700

    Basic port to allow install as a Moodle media player.

---
+++ classes/plugin.php
@@ -0,0 +1,156 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Main class for plugin 'media_ableplayer'
+ *
+ * @package   media_ableplayer
+ * @copyright 2024 Dustin Jeffery
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class media_ableplayer_plugin extends core_media_player_native {
+    public function embed($urls, $name, $width, $height, $options) {
+        $mediamanager = core_media_manager::instance();
+
+        $sources = array();
+        $tracks = array();
+        $text = null;
+        $isaudio = null;
+        $hastracks = false;
+        $hasposter = false;
+
+        if (array_key_exists(core_media_manager::OPTION_ORIGINAL_TEXT, $options) &&
+            preg_match('/^<(video|audio)\b/i', $options[core_media_manager::OPTION_ORIGINAL_TEXT], $matches)) {
+            // Original text already had media tag - get some data from it.
+            $text = $options[core_media_manager::OPTION_ORIGINAL_TEXT];
+            $isaudio = strtolower($matches[1]) === 'audio';
+            $hastracks = preg_match('/<track\b/i', $text);
+            if ($hastracks) {
+                preg_match_all('/(\<track.*?>)/', $text,$originaltracks);
+                foreach($originaltracks[1] as $track) {
+                    $tracks[] = $track;
+                }
+            }
+            $tracks = implode("\n", $tracks);
+            $hasposter = self::get_attribute($text, 'poster') !== null;
+        }
+
+        // Build list of source tags.
+        foreach ($urls as $url) {
+            $mimetype = core_media_manager::instance()->get_mimetype($url);
+            if ($mimetype === 'video/quicktime' && (core_useragent::is_chrome() || core_useragent::is_edge())) {
+                // Set mimetype of quicktime videos to mp4 for Chrome/Edge browsers .
+                $mimetype = 'video/mp4';
+            }
+            $source = html_writer::empty_tag('source', array('src' => $url, 'type' => $mimetype));
+            if ($mimetype === 'video/mp4') {
+                // Better add m4v as first source, it might be a bit more
+                // compatible with problematic browsers
+                array_unshift($sources, $source);
+            } else {
+                $sources[] = $source;
+            }
+        }
+
+        $sources = implode("\n", $sources);
+        $title = $this->get_name($name, $urls);
+        // Escape title but prevent double escaping.
+        $title = s(preg_replace(['/&amp;/', '/&gt;/', '/&lt;/'], ['&', '>', '<'], $title));
+
+        self::pick_video_size($width, $height);
+        if (!$height) {
+            // Let browser choose height automatically.
+            $size = "width=\"$width\"";
+        } else {
+            $size = "width=\"$width\" height=\"$height\"";
+        }
+
+        // We don't want fallback to another player because list_supported_urls() is already smart.
+        // Otherwise we could end up with nested <video> tags. Fallback to link only.
+        $fallback = self::LINKPLACEHOLDER;
+
+        return <<<OET
+<div class="mediaplugin mediaplugin_ableplayer">
+<video data-able-player data-skin="2020" playsinline $size preload="auto" title="$title">
+    $sources
+    $tracks
+    $fallback
+</video>
+</div>
+OET;
+    }
+
+    public function get_supported_extensions() {
+        global $CFG;
+        require_once($CFG->libdir . '/filelib.php');
+        return file_get_typegroup('extension', 'html_video');
+    }
+
+    public function list_supported_urls(array $urls, array $options = array()) {
+        $extensions = $this->get_supported_extensions();
+        $result = array();
+        foreach ($urls as $url) {
+            $ext = core_media_manager::instance()->get_extension($url);
+            if (in_array('.' . $ext, $extensions) && core_useragent::supports_html5($ext)) {
+                // Unfortunately html5 video does not handle fallback properly.
+                // https://www.w3.org/Bugs/Public/show_bug.cgi?id=10975
+                // That means we need to do browser detect and not use html5 on
+                // browsers which do not support the given type, otherwise users
+                // will not even see the fallback link.
+                $result[] = $url;
+            }
+        }
+        return $result;
+    }
+
+     /**
+     * Utility function that sets width and height to defaults if not specified
+     * as a parameter to the function (will be specified either if, (a) the calling
+     * code passed it, or (b) the URL included it).
+     * @param int $width Width passed to function (updated with final value)
+     * @param int $height Height passed to function (updated with final value)
+     */
+    protected static function pick_video_size(&$width, &$height) {
+        global $CFG;
+        if (!$width) {
+            $width = $CFG->media_default_width;
+        }
+    }
+
+    /**
+     * Default rank
+     * @return int
+     */
+    public function get_rank() {
+        return 2000;
+    }
+
+    /**
+     * Setup page requirements
+     * 
+     * @param moodle_page $page The page we are going to add requirements to.
+     */
+    public function setup($page) {
+        $page->requires->jquery();
+        $page->requires->js('/media/player/ableplayer/build/ableplayer.min.js',true);
+        $page->requires->js('/media/player/ableplayer/thirdparty/js.cookie.min.js',true);
+        $page->requires->css('/media/player/ableplayer/build/ableplayer.min.css');
+    }
+
+}
---
+++ lang/en/media_ableplayer.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for plugin 'media_ableplayer'
+ *
+ * @package    media_ableplayer
+ * @copyright  2024 Dustin Jeffery
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Able Player';
+$string['pluginname_help'] = 'An accessible player for video and audio files.';
+$string['privacy:metadata'] = 'The Able Player media plugin does not store any personal data.';
